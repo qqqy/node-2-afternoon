6e5d004c73c721a37926e139caf5703720d080cc
qqqy <frogasaurus@hotmail.com>
2018-12-13 16:19:17 -0700

    read data functions complete

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ require('dotenv').config();
 const {PORT , CONNECTION_STRING} = process.env
 const app = express()
 const writeCtrl = require('./controllers/writeDataController')
+const readCtrl = require('./controllers/readDataController')
 app.use(bodyParser.json())
 
 app.post('/api/create_product' , writeCtrl.create_product)
+app.get('/api/products' , readCtrl.read_products)
 
 massive(CONNECTION_STRING).then(connection => {
   app.set('db' , connection)
